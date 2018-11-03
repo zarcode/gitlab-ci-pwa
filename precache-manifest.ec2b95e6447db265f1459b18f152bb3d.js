24deb85c11a44fe8e9e4ea28ccd51397d02a8b21
Zarko Dencic <denciczarko@gmail.com>
2018-11-03 21:45:30 +0100

    Updates

---
+++ precache-manifest.ec2b95e6447db265f1459b18f152bb3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0360ed6f286d721c92d",
+    "url": "/gitlab-ci-pwa/static/js/runtime~main.c0360ed6.js"
+  },
+  {
+    "revision": "a5ee20cad629f51edf17",
+    "url": "/gitlab-ci-pwa/static/js/main.a5ee20ca.chunk.js"
+  },
+  {
+    "revision": "ee8bad4d37702af09c6f",
+    "url": "/gitlab-ci-pwa/static/js/1.ee8bad4d.chunk.js"
+  },
+  {
+    "revision": "a5ee20cad629f51edf17",
+    "url": "/gitlab-ci-pwa/static/css/main.54f9e960.chunk.css"
+  },
+  {
+    "revision": "faead4cb69f83b6234ecd060a85c39b2",
+    "url": "/gitlab-ci-pwa/index.html"
+  }
+];

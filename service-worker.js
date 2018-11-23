bd0499a35ac71e0cd930542c24be8e66efa6d4c7
Zarko Dencic <denciczarko@gmail.com>
2018-11-23 21:23:17 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gitlab-ci-pwa/precache-manifest.0702892cdfdc537d191cb88d4d75c5a7.js"
+  "/gitlab-ci-pwa/precache-manifest.102393cbd92fb911b21e364282feaf3f.js"
 );
 
 workbox.clientsClaim();

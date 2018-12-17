ae0755eef7433bf314ec0e5923be2ab1ad25d3d9
Zarko Dencic <denciczarko@gmail.com>
2018-12-17 22:07:35 +0100

    cache api

--- src/sw.js
+++ src/sw.js
@@ -1,21 +1,26 @@
 workbox.skipWaiting()
 workbox.clientsClaim()
 
+workbox.routing.registerRoute(
+  new RegExp('https:\/\/gitlab.com\/api\/v4\/.*'),
+  workbox.strategies.networkFirst()
+)
+
 self.addEventListener('fetch', event => {
-    if(event.request.method === 'POST' || event.request.method === 'DELETE') {
-      event.respondWith(
-        fetch(event.request).catch(err => {
-          return new Response(
-            JSON.stringify({ message: 'This action disabled while app is offline' }), 
-            {
-              status: 503,
-              statusText: 'This action disabled while app is offline',
-              headers: { 'Content-Type': 'application/json' }
-            }
-          )
-        })
-      )
-    }
-  })
+  if (event.request.method === 'POST' || event.request.method === 'DELETE') {
+    event.respondWith(
+      fetch(event.request).catch(err => {
+        return new Response(
+          JSON.stringify({ message: 'This action disabled while app is offline' }),
+          {
+            status: 503,
+            statusText: 'This action disabled while app is offline',
+            headers: { 'Content-Type': 'application/json' }
+          }
+        )
+      })
+    )
+  }
+})
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest || [])
